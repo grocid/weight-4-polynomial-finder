52339f0dfc209ba874022049e7a56b7adad83220
Francisco Blas (klondike) Izquierdo Riera <klondike@klondike.es>
2021-04-15 01:29:42 +0200

    Optimize monomial handling functions, this makes them around 5% faster

--- polyfinder/gf2_monomial.cpp
+++ polyfinder/gf2_monomial.cpp
@@ -1,6 +1,28 @@
 typedef unsigned __int128 uint128_t;
 
-inline uint32_t get_degree(uint128_t px)
+
+#ifdef __GNUC__
+#define PUREFUN [[gnu::pure]]
+#define likely(x)       __builtin_expect((x),1)
+#define unlikely(x)     __builtin_expect((x),0)
+#else
+#define PUREFUN
+#define likely(x)       (x)
+#define unlikely(x)     (x)
+#endif
+
+#if __GNUC__
+PUREFUN inline uint32_t get_degree(uint128_t px)
+{
+    unsigned long long hi = px >> 64, lo = px;
+    //NOTE: DO NOT REMOVE the != if you do this becomes a long on 32-bits and results get messed up!
+    if (likely(hi != 0))
+        return 127-__builtin_clzll(hi);
+    else
+        return 63-__builtin_clzll(lo|1);
+}
+#else
+PUREFUN inline uint32_t get_degree(uint128_t px)
 {
     uint32_t degree = 0;
 
@@ -15,64 +37,45 @@ inline uint32_t get_degree(uint128_t px)
 
     return degree;
 }
+#endif
+
+PUREFUN inline uint128_t next_monomial(uint128_t px, uint128_t f, uint128_t poly)
+{
+    px <<= 1;
+    //NOTE: DO NOT REMOVE the != if you do this becomes a long and results get messed up!
+    if (likely((px & f) != 0))
+        px ^= poly;
+    return px;
+}
+
 
 /*
     GF(2) polynomial multiplication mod P(x)
 */
-inline uint128_t gf2x_multiply(uint128_t a, uint128_t b, uint128_t f, uint128_t poly)
+PUREFUN inline uint128_t gf2x_multiply(uint128_t a, uint128_t b, uint128_t f, uint128_t poly)
 {
     uint128_t q = b, r = 0;
 
     for (int i = 0; i < 128; ++i)
     {
-        if ((a & ((uint128_t)1 << i)) != 0)
-        {
+        if ((a & ((uint128_t)1 << i)))
             r ^= q;
-        }
-        q = (q << 1);
-        if ((q & f) != 0)
-        {
-            q ^= poly;
-        }
+        q = next_monomial(q,f,poly);
     }
-
     return r;
 }
 
 /*
     GF(2) polynomial exponentiation mod P(x)
 */
-inline uint128_t gf2x_exp(uint128_t x, uint128_t n, uint128_t poly)
+PUREFUN inline uint128_t gf2x_exp(uint128_t x, uint128_t n, uint128_t poly)
 {
-    if (n == 0)
-    {
-        return 1;
-    }
-
     uint128_t y = 1, f = (((uint128_t) 1) << get_degree(poly));
-    while (n > 1)
+    for (;n != 0;n >>= 1)
     {
-        if (n % 2 == 0)
-        {
-            x = gf2x_multiply(x, x, f, poly);
-            n = n / 2;
-        }
-        else
-        {
+        if (n & 1) // n %2  == 1
             y = gf2x_multiply(x, y, f, poly);
-            x = gf2x_multiply(x, x, f, poly);
-            n = (n - 1) / 2;
-        }
+        x = gf2x_multiply(x, x, f, poly);
     }
-
-    return gf2x_multiply(x, y, f, poly);
-}
-
-inline uint128_t next_monomial(uint128_t px, uint128_t f, uint128_t poly)
-{
-    px <<= 1;
-    if ((px & f) != 0){
-        px ^= poly;
-    }
-    return px;
+    return y;
 }
