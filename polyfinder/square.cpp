b9346790be79506ee8cb6d3a78208eed948d47e3
Francisco Blas (klondike) Izquierdo Riera <klondike@klondike.es>
2021-04-15 01:31:19 +0200

    For now, use a fixed monomial and seed to make development a bit easier

--- polyfinder/square.cpp
+++ polyfinder/square.cpp
@@ -23,7 +23,8 @@
 #define THREADS 23
 #define BETA    1
 #define ALPHA   1
-#define SEED    (time(NULL)+1)
+//#define SEED    (time(NULL)+1)
+#define SEED    (1618440951)
 
 //#define DEBUG_MESSAGES 1
 #define IN_MEM_GENERATION 1
@@ -31,9 +32,10 @@
 // LILI-128
 //#define POLY ((((uint128_t) 257) << 64) + (uint128_t)(299733420677929))
 // Bluetooth E0 x^1243366916+x^210123252+x^139501803+1
-#define POLY ((((uint128_t) 0x2090000) << 64) + 0xa0048000000003)
+//#define POLY ((((uint128_t) 0x2090000) << 64) + 0xa0048000000003)
 // Bluetooth E0
 //#define POLY ((((uint128_t) 0x212011231) << 64) + 0x2fea1a9dc028a229)
+#define POLY ((((uint128_t)1) << 80)|(0xa195150d15*2+1))
 
 #define LAYER_SIZE (THREADS*THREADS)
 
