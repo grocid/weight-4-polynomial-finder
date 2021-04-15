17581fd56f3f1914178597b337ade8e3c025e754
Francisco Blas (klondike) Izquierdo Riera <klondike@klondike.es>
2021-04-15 14:03:59 +0200

    Extract the mask and the inverse mask, use only the needed bits

--- polyfinder/square.cpp
+++ polyfinder/square.cpp
@@ -16,26 +16,12 @@
 #include <boost/archive/binary_iarchive.hpp>
 #include <boost/serialization/vector.hpp>
 
+#include "config.h"
+
 #include "gf2_monomial.cpp"
 #include "common.cpp"
 #include "stringops.cpp"
 
-#define THREADS 23
-#define BETA    1
-#define ALPHA   1
-//#define SEED    (time(NULL)+1)
-#define SEED    (1618440951)
-
-//#define DEBUG_MESSAGES 1
-#define IN_MEM_GENERATION 1
-
-// LILI-128
-//#define POLY ((((uint128_t) 257) << 64) + (uint128_t)(299733420677929))
-// Bluetooth E0 x^1243366916+x^210123252+x^139501803+1
-//#define POLY ((((uint128_t) 0x2090000) << 64) + 0xa0048000000003)
-// Bluetooth E0
-//#define POLY ((((uint128_t) 0x212011231) << 64) + 0x2fea1a9dc028a229)
-#define POLY ((((uint128_t)1) << 80)|(0xa195150d15*2+1))
 
 #define LAYER_SIZE (THREADS*THREADS)
 
@@ -46,18 +32,166 @@
 using namespace std;
 typedef unsigned __int128 uint128_t;
 
+static constexpr uint32_t polynomial_degree = get_degree(POLY);
+static constexpr uint64_t total_map_size = (uint64_t)(((uint64_t)1 << (polynomial_degree/3 + BETA)) * ALPHA);
+static constexpr uint64_t exp_len = (get_degree(total_map_size) + 8) / 8;
+static constexpr uint64_t masklen = polynomial_degree/3-2;
+static constexpr uint64_t imasklen = (polynomial_degree+1)-masklen;
+static constexpr uint64_t masklenb = (masklen+7)/8;
+static constexpr uint64_t imasklenb = (imasklen+7)/8;
+
+static uint128_t mask;
+
+inline uint32_t phi(uint128_t val)
+{
+    return val % THREADS;
+}
+
+//Find the relative positions of the mask bits
+inline void gen_mask_bits(uint128_t mask, int maskbits[128], uint32_t f, bool match) {
+    int masklen = 0;
+    int last = 0;
+    for (int i = 0; i < (int)f+1; i++) {
+        if((mask & 1) == match) {
+            maskbits[masklen++]=i-last;
+            last = i;
+        }
+        mask >>= 1;
+    }
+}
+
+//Extract only the masked bits
+inline uint128_t get_mask_bits(const int masklen, const int maskbits[128], uint128_t px) {
+    uint128_t rv = 0;
+    for (int i = 0; i < masklen; i++) {
+        px >>= maskbits[i];
+        rv |= (((uint64_t)px)&1)<<i;
+    }
+    return rv;
+}
+
+#ifdef __GNUC__
+#define PACKSTRUCT __attribute__((__packed__))
+#else
+#define PACKSTRUCT
+#endif
+
+typedef struct PACKSTRUCT exp_t {
+    uint8_t exponent[exp_len];
+} exp_t;
+
+typedef struct PACKSTRUCT mask_t {
+    uint8_t mask[masklenb];
+} mask_t;
+
+typedef struct PACKSTRUCT imask_t {
+    uint8_t imask[imasklenb];
+} imask_t;
+
+typedef struct PACKSTRUCT cmap_poly {
+    imask_t imask;
+    exp_t exponent;
+} cmap_poly;
+
+typedef struct PACKSTRUCT clay_poly {
+    exp_t e1;
+    exp_t e2;
+} clay_poly;
+
+PUREFUN constexpr inline exp_t pack_exp(uint64_t exp) {
+    exp_t rv = {};
+    for (uint i = 0; i < exp_len; i++)
+        rv.exponent[i] = exp >> (8*i);
+    return rv;
+}
+
+
+PUREFUN constexpr inline uint64_t unpack_exp(const exp_t &exp) {
+    uint64_t rv = 0;
+    for (uint i = 0; i < exp_len; i++)
+        rv |= ((uint64_t)exp.exponent[i]) <<  (8*i);
+    return rv;
+}
+
+PUREFUN constexpr inline mask_t pack_mask(uint128_t mask) {
+    mask_t rv = {};
+    for (uint i = 0; i < masklenb; i++)
+        rv.mask[i] = mask >> (8*i);
+    return rv;
+}
+
+
+PUREFUN constexpr inline uint128_t unpack_mask(const mask_t &mask) {
+    uint128_t rv = 0;
+    for (uint i = 0; i < masklenb; i++)
+        rv |= ((uint128_t)mask.mask[i]) <<  (8*i);
+    return rv;
+}
+
+PUREFUN constexpr inline imask_t pack_imask(uint128_t imask) {
+   imask_t rv = {};
+    for (uint i = 0; i < imasklenb; i++)
+        rv.imask[i] = imask >> (8*i);
+    return rv;
+}
+
+
+PUREFUN constexpr inline uint128_t unpack_imask(const imask_t &imask) {
+    uint128_t rv = 0;
+    for (uint i = 0; i < imasklenb; i++)
+        rv |= ((uint128_t)imask.imask[i]) <<  (8*i);
+    return rv;
+}
+
+PUREFUN constexpr inline imask_t xor_imask(const imask_t &imask1, const imask_t &imask2) {
+    imask_t rv = {};
+    for (uint i = 0; i < imasklenb; i++)
+        rv.imask[i] = imask1.imask[i] ^ imask2.imask[i];
+    return rv;
+}
+
+
 namespace std {
     template<>
-    struct hash<uint128_t>
+    struct hash<mask_t>
     {
-       typedef uint128_t argument_type;
-       typedef size_t result_type;
-
-       result_type operator () (const argument_type& x) const
-       {
-          return boost::hash<uint128_t>()(static_cast<uint128_t>(x));
-       }
+        typedef mask_t argument_type;
+        typedef size_t result_type;
+        PUREFUN constexpr inline result_type operator () (const argument_type& x) const
+        {
+            result_type rv = 0;
+            for (uint i = 0; i < masklenb; i++) {
+                rv = rv << 8 | rv >> (sizeof(result_type)-1*8);
+                rv ^= x.mask[i];
+            }
+            return rv;
+        }
     };
+    template<>
+    struct hash<imask_t>
+    {
+        typedef imask_t argument_type;
+        typedef size_t result_type;
+        PUREFUN constexpr inline result_type operator () (const argument_type& x) const
+        {
+            result_type rv = 0;
+            for (uint i = 0; i < imasklenb; i++) {
+                rv = rv << 8 | rv >> (sizeof(result_type)-1*8);
+                rv ^= x.imask[i];
+            }
+            return rv;
+        }
+    };
+}
+
+PUREFUN constexpr inline bool operator==(const mask_t& lhs, const mask_t& rhs)
+{
+    return !memcmp(lhs.mask, rhs.mask, masklenb);
+}
+
+PUREFUN constexpr inline bool operator==(const imask_t& lhs, const imask_t& rhs)
+{
+    return !memcmp(lhs.imask, rhs.imask, imasklenb);
 }
 
 #define map_t phmap::flat_hash_map
@@ -67,27 +201,25 @@ namespace std {
 
 template <class K, class V> using layer = map_t<K, V>;
 
-map_t<uint128_t, tuple<uint64_t,  uint64_t>> collision_layer[LAYER_SIZE];
+map_t<imask_t, clay_poly> collision_layer[LAYER_SIZE];
 
-static uint64_t total_map_size;
-static uint32_t polynomial_degree;
-static uint128_t mask;
-
-inline uint32_t phi(uint128_t val)
-{
-    return val % THREADS;
-}
 
 void in_memory_generate(uint128_t mask, uint32_t thread)
 {
     uint64_t exponent = 0;
-    uint32_t idx, added = 0;
-    uint128_t py, px = 1;
-    uint128_t f = (((uint128_t) 1) << get_degree(POLY));
+    uint32_t idx;
+#ifdef DEBUG_MESSAGES
+    uint32_t added = 0;
+#endif
+    uint128_t px = 1;
+    const uint32_t f = get_degree(POLY);
+    int maskbits[masklen];
+    int imaskbits[imasklen];
+    gen_mask_bits(mask, maskbits,f,1);
+    gen_mask_bits(mask, imaskbits,f,0);
 
-    map_t<uint128_t, tuple<uint128_t, uint64_t>> collision_map;
-    map_t<uint128_t, tuple<uint128_t, uint64_t>>::iterator it;
-
+    map_t<mask_t, cmap_poly> collision_map;
+    
     for(uint64_t i = 0; i < total_map_size; ++i)
     {
         // every thread considers each monomial in the range
@@ -95,38 +227,42 @@ void in_memory_generate(uint128_t mask, uint32_t thread)
         // the phi condition.
         idx = phi(px & mask);
 
-        if (idx == thread)
+        if (unlikely(idx == thread))
         {
+            exp_t pexp = pack_exp(exponent);
+            mask_t mbits = pack_mask(get_mask_bits(masklen,maskbits,px));
+            imask_t imbits = pack_imask(get_mask_bits(imasklen,imaskbits,px));
             auto [it, result] = collision_map.try_emplace(
-                px & mask, tuple{px, exponent}
+                mbits, cmap_poly{imbits, pexp}
             );
 
-            if(!result)
+            if(unlikely(!result))
             {
-                py = get<0>(it->second);                
-                auto [it2, result] = collision_layer[THREADS * thread + phi(px ^ py)].try_emplace(
-                    py ^ px, tuple{exponent, get<1>(it->second)}
+                imask_t imaskxor = xor_imask(it->second.imask, imbits);
+                uint128_t py = unpack_imask(imaskxor);
+                exp_t exponent2 = it->second.exponent;
+                auto [it2, result] = collision_layer[THREADS * thread + phi(py)].try_emplace(
+                    imaskxor, clay_poly{pexp, exponent2}
                 );
 #ifdef DEBUG_MESSAGES
                 added++;
 #endif
                 // This is rather unlikely to happen, but if we don't check
                 // then we might miss a multiple.
-                if(!result)
+                if(unlikely(!result))
                 {
                     vector<uint64_t> exponents;
-                    exponents.push_back(exponent);
-                    exponents.push_back(get<1>(it->second));
-                    exponents.push_back(get<0>(it2->second));
-                    exponents.push_back(get<1>(it2->second));
+                    exponents.push_back(unpack_exp(pexp));
+                    exponents.push_back(unpack_exp(exponent2));
+                    exponents.push_back(unpack_exp(it2->second.e1));
+                    exponents.push_back(unpack_exp(it2->second.e2));
 
                     cout << polynomial_representation(exponents).str() << endl;
-                    return;
                 }
             }
         }
 
-        px = next_monomial(px, f, POLY);
+        px = next_monomial(px, POLY, f);
         exponent++;
     }
 #ifdef DEBUG_MESSAGES
@@ -149,10 +285,10 @@ void in_memory_merge(uint128_t mask, int thread)
             if(!result)
             {
                 vector<uint64_t> exponents;
-                exponents.push_back(get<0>(it.second));
-                exponents.push_back(get<1>(it.second));
-                exponents.push_back(get<0>(it2->second));
-                exponents.push_back(get<1>(it2->second));
+                exponents.push_back(unpack_exp(it.second.e1));
+                exponents.push_back(unpack_exp(it.second.e2));
+                exponents.push_back(unpack_exp(it2->second.e1));
+                exponents.push_back(unpack_exp(it2->second.e2));
 
                 cout << polynomial_representation(exponents).str() << endl;
             }
@@ -170,7 +306,7 @@ void in_memory_search (uint128_t mask)
 #endif
 
     cout << "Generating 2^" << log2(total_map_size) << " monomials..." << endl;
-
+    cout << "Using " << (exp_len*8) << " bits for exponents..." << endl;
     thread t[THREADS];    
     for (uint32_t i = 0; i < THREADS; ++i) {
         t[i] = thread(in_memory_generate, mask, i);
@@ -248,7 +384,7 @@ void sequential_generate(uint128_t mask, uint64_t thread, uint64_t starting_expo
     uint64_t idx;
     uint64_t exponent = starting_exponent;
     uint128_t px = gf2x_exp(2, exponent, POLY);
-    uint128_t f = (((uint128_t) 1) << polynomial_degree);
+    uint32_t f = polynomial_degree;
 
     vector<element_t> sequential;
 
@@ -261,7 +397,7 @@ void sequential_generate(uint128_t mask, uint64_t thread, uint64_t starting_expo
             sequential.push_back({px, exponent, 0});
         }
 
-        px = next_monomial(px, f, POLY);
+        px = next_monomial(px, POLY, f);
         exponent++;
     }
 
@@ -306,9 +442,7 @@ void thread_merge()
 
 int main() 
 { 
-    polynomial_degree = get_degree(POLY);
     mask = random_mask(0, polynomial_degree/3-2, polynomial_degree, SEED);
-    total_map_size = (uint64_t)(((uint64_t)1 << (polynomial_degree/3 + BETA)) * ALPHA);
 
     cout << "Polynomial:  " << polynomial_representation(POLY).str() << endl;
     cout << "Degree:      " << polynomial_degree << endl;
