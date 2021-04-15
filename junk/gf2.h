ba38b82270103d843c9bcaa902f673e57e8c29b1
Carl Löndahl <carl@grocid.net>
2021-04-15 20:46:59 +0200

    Remove the old broken stuff

--- junk/gf2.h
+++
@@ -1,7 +0,0 @@
-#include <stdint.h>
-#include <inttypes.h>
-
-typedef unsigned __int128 uint128_t;
-
-uint128_t multiply(uint128_t a, uint128_t b, uint128_t f, uint128_t poly, int degree);
-uint128_t gf2_exp(uint128_t a, uint128_t b, uint128_t f, uint128_t poly, int degree);
